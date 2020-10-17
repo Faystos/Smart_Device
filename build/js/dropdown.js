e8796a42bafb4091d971995c001072969c9ae4f1
Vladimir <fayst_@bk.ru>
2020-10-17 22:18:43 +0300

    Доработал аккардеон, занимаюсь устранение замечаний.

--- build/js/dropdown.js
+++ build/js/dropdown.js
@@ -4,39 +4,43 @@ class Dropdown {
   constructor(btn, block1, block2) {
     this.btn = document.querySelectorAll(`.${btn}`);
     this.classBtn = btn;
-    this.block1 = document.querySelectorAll(`.${block1}`);
+    this.block1 = document.querySelector(`.${block1}`);
     this.classBlock1 = block1;
-    this.block2 = document.querySelectorAll(`.${block2}`);
+    this.block2 = document.querySelector(`.${block2}`);
     this.classBlock2 = block2;
     this.initDropdown();
   }
 
-  initDropdown () {
+  initDropdown () {    
     let classBtn = this.classBtn;
     let block1 = this.block1;
     let block2 = this.block2;
     let classBlock1 = this.classBlock1;
     let classBlock2 = this.classBlock2;
-    this.btn.forEach(el => {
-      el.addEventListener('click', evt => {       
-      this.toggleClass(evt.target, classBtn);
-      this.closeBlock(block2, classBlock2);
-      });
+    this.btn.forEach((el, i, arr) => {
+      el.addEventListener('click', evt => {
+        this.toggleBtn(arr, el, classBtn); 
+        if (evt.target.dataset.block === classBlock1) this.toggleBlock(block1, block2, classBlock1, classBlock2);
+        if (evt.target.dataset.block === classBlock2) this.toggleBlock(block2, block1, classBlock2, classBlock1);                          
+      });      
     });
   }
 
-  toggleClass = (el, classEl) => {
-    el.classList.toggle(`${classEl}--open`);
-    el.classList.toggle(`${classEl}--close`);
-  }
+  toggleBtn = (btns, target, classBtn) => {  
+    if (target.classList.contains(`${classBtn}--open`)) {
+      target.classList.remove(`${classBtn}--open`);
+    } else {
+        btns.forEach(btn => {      
+            btn.classList.remove((`${classBtn}--open`));
+            target.classList.add(`${classBtn}--open`);    
+        });  
+    }  
+  }  
 
-  closeBlock(block, blockClass) {
-    if(block.classList.contains(`${blockClass}--open`)) {
-      block.classList.remove(`${blockClass}--open`);
-      block.classList.add(`${blockClass}--close`);
-    }
-
-  }
+  toggleBlock = (blockOpen, blockClose, classBlockOpen, classBlockClose) => {
+    if (blockClose.classList.contains(`${classBlockClose}--open`))blockClose.classList.remove(`${classBlockClose}--open`); 
+    blockOpen.classList.toggle(`${classBlockOpen}--open`);
+  }  
 }
 
 new Dropdown('footer__btn', 'footer__sesection_link', 'footer__addres_text');
